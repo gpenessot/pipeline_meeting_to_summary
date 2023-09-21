e9d725f6c5b7fc815286ff2eef2cf390b61ba8a0
Gaël Penessot <gael.penessot@gmail.com>
2023-09-21 23:13:51 +0200

    update extract_audio.py

--- extract_audio.py
+++ extract_audio.py
@@ -1,4 +1,18 @@
 import moviepy.editor as mp
+import sys
 
-rn = my_clip = mp.VideoFileClip('reunion_des_referents_20230913.mp4')
-my_clip.audio.write_audiofile('reunion_des_referents_20230913.wav')
+def extract_audio_from_video(video_path, 
+                             audio_format='wav'):
+    my_clip = mp.VideoFileClip(video_path)
+    name = video_path.split('.')[0]
+    print('name : ', name)
+    my_clip.audio.write_audiofile(name+'.'+audio_format, 
+                                  codec='pcm_s16le')
+    
+if __name__ == '__main__':
+    print(sys.argv[1])
+    print(sys.argv[2])
+    extract_audio_from_video(video_path=sys.argv[1],
+                             audio_format=sys.argv[2])
+    
+    
