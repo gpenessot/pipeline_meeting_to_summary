f26d942d405cec0bb00527fabd37d9cff7b1ff2d
Gaël Penessot <gael.penessot@gmail.com>
2023-10-03 00:35:21 +0200

    update scripts

--- pipeline_luigi.py
+++ pipeline_luigi.py
@@ -1,30 +1,37 @@
 import luigi
-import subprocess
 
-# Define a Luigi Task to Extract Audio
-class ExtractAudioTask(luigi.Task):
-    video_path = luigi.Parameter()
-
-    def output(self):
-        return luigi.LocalTarget("audio.mp3")
-
-    def run(self):
-        # Replace this command with your actual extraction command
-        extraction_command = ["python", "extract_audio.py", self.video_path, "audio.mp3"]
-        subprocess.run(extraction_command, check=True)
-
-# Define a Luigi Task to Convert Audio to Text
-class ConvertAudioToTextTask(luigi.Task):
+class ExtractAudio(luigi.Task):
     def requires(self):
-        return ExtractAudioTask(video_path="video.mp4")
-
-    def output(self):
-        return luigi.LocalTarget("output.txt")
+        return []
 
     def run(self):
-        # Replace this command with your actual conversion command
-        conversion_command = ["python", "convert_audio_to_text.py", "audio.mp3", "output.txt"]
-        subprocess.run(conversion_command, check=True)
+        import subprocess
+        subprocess.run(["python", "extract_audio.py"])
+
+class ConvertAudioToText(luigi.Task):
+    def requires(self):
+        return [ExtractAudio()]
+
+    def run(self):
+        import subprocess
+        subprocess.run(["python", "convert_audio_to_text.py"])
+
+class Punctuation(luigi.Task):
+    def requires(self):
+        return [ConvertAudioToText()]
+
+    def run(self):
+        import subprocess
+        subprocess.run(["python", "punctation.py"])
+
+class Summary(luigi.Task):
+    def requires(self):
+        return [Punctuation()]
+
+    def run(self):
+        import subprocess
+        subprocess.run(["python", "summary.py"])
 
 if __name__ == "__main__":
-    luigi.build([ConvertAudioToTextTask()], local_scheduler=True)
+    luigi.build([ExtractAudio(), ConvertAudioToText(), Punctuation(), Summary()], local_scheduler=True)
+    luigi.run()
--- punctuation.py
+++ punctuation.py
@@ -3,10 +3,7 @@ import os
 import glob
 
 model = PunctuationModel()
-#text = "My name is Clara and I live in Berkeley California Ist das eine Frage Frau Müller"
-#result = model.restore_punctuation(text)
-            
-            
+
 if __name__ == '__main__':
     # Create a directory to save the audio chunks
 
@@ -20,7 +17,6 @@ if __name__ == '__main__':
         print(f'fichier en cours {file} \n')
         with open(os.path.join(output_dir, f"text_with_punctation_{i}.txt"), 'w') as f:
             with open(file, 'r') as content:
-                #print(f'{content.read()}\n\n')
                 result=model.restore_punctuation(content.read())
                 f.write(result)
         f.close()
